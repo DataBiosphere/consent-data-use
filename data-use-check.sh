eb91ef2cf53f4ca3207a694c9df9c87aed8592f0
Greg Rushton <grushton@broadinstitute.org>
2017-09-14 12:50:51 -0400

    fix broken validation

--- data-use-check.sh
+++ data-use-check.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-sudo apt-get install jq
-sudo apt-get install curl
-response=$(curl -sb -H "Accept:application/json" http://swse.deri.org/RDFAlerts/alerts?url=https://storage.googleapis.com/broad-dsde-consent-dev/ont/organization/data-use.owl"&"format=json)
-filled=$(echo $response | jq '.results | has(0)' )
-if (echo $response | grep -q "okay");
+
+ONTOLOGY_FILE=$1
+
+response=$(curl 'http://mowl-power.cs.man.ac.uk:8080/validator/validate' \
+                -H 'Content-Type: application/x-www-form-urlencoded' \
+                --data-urlencode "ontology@${ONTOLOGY_FILE}" \
+                --data "reportsyntax=Manchester" \
+                --data "profile=OWL2")
+
+if (echo $response | grep "The ontology and all of its imports are in the OWL 2 profile");
 then
-   echo "SWSE Service responded successfully."
+   echo "Validation service responded successfully."
 else
-   echo "Something went wrong with the online validator service. Please check."
-   retval=1
+   echo "Something went wrong with the online validator service. Please manually validate against http://mowl-power.cs.man.ac.uk:8080/validator/validate."
+   exit 1
 fi
-if [ "$filled" == "true" ]; then
-    echo "Unsuccessful parsing of data-use.owl file."
-    retval=1
-else
-    echo "Successful parsing of data-use.owl file."
-    retval=0
-fi
-return $retval
